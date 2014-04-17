32c08cd5fa04c518bc526c0037207d7a199e7c99
Evan Ram <evanram@users.noreply.github.com>
2014-04-17 12:50:27 -0500

    Added javadoc

--- Chat_Server/src/com/evanram/code/java/chatserver/command/CommandNick.java
+++ Chat_Server/src/com/evanram/code/java/chatserver/command/CommandNick.java
@@ -5,6 +5,9 @@ import com.evanram.code.java.chatserver.Commander;
 import com.evanram.code.java.chatserver.RunCommand;
 import com.evanram.code.java.chatserver.Server;
 
+/**
+ * Command to change your nickname.
+ */
 public class CommandNick implements RunCommand
 {
 
