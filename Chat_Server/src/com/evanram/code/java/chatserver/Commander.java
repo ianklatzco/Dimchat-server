dfe9adeaa708a75812c86d21f0a95945345e9781
Evan Ram <evanram@users.noreply.github.com>
2014-04-17 12:53:53 -0500

    Added javadoc

--- Chat_Server/src/com/evanram/code/java/chatserver/Commander.java
+++ Chat_Server/src/com/evanram/code/java/chatserver/Commander.java
@@ -1,5 +1,9 @@
 package com.evanram.code.java.chatserver;
 
+/**
+ * A commander is anyone who can send commands.
+ * Both CommandServer and SessionWorker are Commanders because they can send commands to the server.
+ */
 public interface Commander
 {
 	boolean isOp();
